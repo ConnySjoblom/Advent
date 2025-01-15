065be992eef948536bcc144bbb76242aa53cced8
Conny Sjöblom <conny@sjoblom.io>
2025-01-15 22:32:10 +0200

    Add tests for 2019 Day 1

---
+++ tests/Feature/Year2019/Day01Test.php
@@ -0,0 +1,25 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    $this->solution = new \App\Solutions\Year2019\Day01;
+});
+
+test('Day 01 Part 1', function (string $input, string $answer) {
+    $this->solution->input = $input;
+    assertEquals($answer, $this->solution->partOne());
+})->with([
+    ['12', '2'],
+    ['14', '2'],
+    ['1969', '654'],
+    ['100756', '33583'],
+]);
+
+test('Day 01 Part 2', function (string $input, string $answer) {
+    $this->solution->input = $input;
+    assertEquals($answer, $this->solution->partTwo());
+})->with([
+    ['1969', '966'],
+    ['100756', '50346'],
+]);
