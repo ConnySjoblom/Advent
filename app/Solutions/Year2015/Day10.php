effc3ceb57de0b52b6efe5709e379e1958b0672a
Conny Sjöblom <conny@sjoblom.io>
2025-01-16 22:19:50 +0200

    Add 2015 Day 10

---
+++ app/Solutions/Year2015/Day10.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Solutions\Year2015;
+
+use App\Solutions\Solution;
+
+class Day10 extends Solution
+{
+    /**
+     * Day 10 Part 1
+     */
+    public function partOne(): ?string
+    {
+        $times = 40;
+        $input = trim($this->input);
+
+        for ($i = 0; $i < $times; $i++) {
+            $input = $this->lookAndSay($input);
+        }
+
+        return strlen($input);
+    }
+
+    /**
+     * Day 10 Part 2
+     */
+    public function partTwo(): ?string
+    {
+        $times = 50;
+        $input = trim($this->input);
+
+        for ($i = 0; $i < $times; $i++) {
+            $input = $this->lookAndSay($input);
+        }
+
+        return strlen($input);
+    }
+
+    public function lookAndSay(string $input): string
+    {
+        preg_match_all('/((\d)\2*)/', $input, $matches, PREG_SET_ORDER, 0);
+
+        $result = '';
+        foreach ($matches as $match) {
+            $result .= sprintf('%d%s', strlen($match[0]), $match[2]);
+        }
+
+        return $result;
+    }
+}
---
+++ tests/Unit/Year2015/Day10Test.php
@@ -0,0 +1,17 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    $this->solution = new \App\Solutions\Year2015\Day10;
+});
+
+test('Day 10 Part 1 & 2', function (string $input, string $answer) {
+    assertEquals($answer, $this->solution->lookAndSay($input));
+})->with([
+    ['1', '11'],
+    ['11', '21'],
+    ['21', '1211'],
+    ['1211', '111221'],
+    ['111221', '312211'],
+]);
