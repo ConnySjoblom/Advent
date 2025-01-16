57ec860d8944719a52af0e9373e7c327af9b8685
Conny Sjöblom <conny@sjoblom.io>
2025-01-16 21:42:02 +0200

    Add 2015 Day 9

---
+++ app/Solutions/Year2015/Day09.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Solutions\Year2015;
+
+use App\Solutions\Solution;
+
+class Day09 extends Solution
+{
+    /**
+     * Day 09 Part 1
+     */
+    public function partOne(): ?string
+    {
+        $input = explode("\n", trim($this->input));
+
+        $places = [];
+        $distances = [];
+
+        foreach ($input as $trip) {
+            preg_match('/^(.*) to (.*) = (.*)$/', $trip, $matches);
+
+            $places[] = $matches[1];
+            $places[] = $matches[2];
+
+            $distance = intval($matches[3]);
+            $distances[$matches[1]][$matches[2]] = $distance;
+            $distances[$matches[2]][$matches[1]] = $distance;
+        }
+
+        $places = array_values(array_unique($places));
+        $trips = $this->generateTrips($places);
+        $shortest = PHP_INT_MAX;
+        foreach ($trips as $trip) {
+            $distance = 0;
+            $destinations = explode('-', $trip);
+
+            if (count(array_unique($destinations)) != count($places)) {
+                continue;
+            }
+
+            for ($i = 0; $i < count($destinations) - 1; $i++) {
+                $distance += $distances[$destinations[$i]][$destinations[$i + 1]];
+            }
+
+            if ($distance < $shortest) {
+                $shortest = $distance;
+            }
+        }
+
+        return sprintf('%d', $shortest);
+    }
+
+    /**
+     * Day 09 Part 2
+     */
+    public function partTwo(): ?string
+    {
+        $input = explode("\n", trim($this->input));
+
+        $places = [];
+        $distances = [];
+
+        foreach ($input as $trip) {
+            preg_match('/^(.*) to (.*) = (.*)$/', $trip, $matches);
+
+            $places[] = $matches[1];
+            $places[] = $matches[2];
+
+            $distance = intval($matches[3]);
+            $distances[$matches[1]][$matches[2]] = $distance;
+            $distances[$matches[2]][$matches[1]] = $distance;
+        }
+
+        $places = array_values(array_unique($places));
+        $trips = $this->generateTrips($places);
+        $longest = 0;
+        foreach ($trips as $trip) {
+            $distance = 0;
+            $destinations = explode('-', $trip);
+
+            if (count(array_unique($destinations)) != count($places)) {
+                continue;
+            }
+
+            for ($i = 0; $i < count($destinations) - 1; $i++) {
+                $distance += $distances[$destinations[$i]][$destinations[$i + 1]];
+            }
+
+            if ($distance > $longest) {
+                $longest = $distance;
+            }
+        }
+
+        return sprintf('%d', $longest);
+    }
+
+    private function generateTrips(array $places): array
+    {
+        if (count($places) <= 1) {
+            $result = $places;
+        } else {
+            $result = [];
+
+            for ($i = 0; $i < count($places); $i++) {
+                $place = $places[$i];
+                $remainingPlaces = [];
+
+                for ($j = 0; $j < count($places); $j++) {
+                    if ($i != $j) {
+                        $remainingPlaces[] = $places[$j];
+                    }
+                }
+
+                $trips = $this->generateTrips($remainingPlaces);
+
+                for ($j = 0; $j < count($trips); $j++) {
+                    $result[] = $place.'-'.$trips[$j];
+                }
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ tests/Unit/Year2015/Day09Test.php
@@ -0,0 +1,31 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    $this->solution = new \App\Solutions\Year2015\Day09;
+});
+
+test('Day 09 Part 1', function (string $input, string $answer) {
+    $this->solution->input = $input;
+    assertEquals($answer, $this->solution->partOne());
+})->with([
+    [<<<'EOF'
+London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141
+EOF
+        , '605'],
+]);
+
+test('Day 09 Part 2', function (string $input, string $answer) {
+    $this->solution->input = $input;
+    assertEquals($answer, $this->solution->partTwo());
+})->with([
+    [<<<'EOF'
+London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141
+EOF
+        , '982'],
+]);
