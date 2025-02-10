3bbfb937fe098a98d46fbc31c8e88acf6d8527f5
Conny Sjöblom <conny@sjoblom.io>
2025-02-10 18:29:13 +0200

    Add Solution for Year 2017 Day 1

---
+++ app/Solutions/Year2017/Day01.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Solutions\Year2017;
+
+use App\Solutions\Solution;
+
+class Day01 extends Solution
+{
+    /**
+     * Day 01 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $input = collect(str_split($this->input))
+            ->map(fn ($letter) => intval($letter))
+            ->toArray();
+
+        $sum = 0;
+        for ($i = 0; $i < count($input); $i++) {
+            $a = $input[$i];
+            $b = $input[($i + 1) % count($input)];
+
+            if ($a === $b) {
+                $sum += $a;
+            }
+        }
+
+        return $sum;
+    }
+
+    /**
+     * Day 01 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        $input = collect(str_split($this->input))
+            ->map(fn ($letter) => intval($letter))
+            ->toArray();
+
+        $sum = 0;
+        for ($i = 0; $i < count($input); $i++) {
+            $a = $input[$i];
+            $b = $input[($i + (count($input) / 2)) % count($input)];
+
+            if ($a === $b) {
+                $sum += $a;
+            }
+        }
+
+        return $sum;
+    }
+}
