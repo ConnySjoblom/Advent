a95482889e117345ceb7cd6bfa8a87ae3fde3bf7
Conny Sjöblom <conny@sjoblom.io>
2025-02-12 20:57:00 +0200

    Add Solution for Year 2017 Day 5

---
+++ app/Solutions/Year2017/Day05.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Solutions\Year2017;
+
+use App\Solutions\Solution;
+
+class Day05 extends Solution
+{
+    /**
+     * Day 05 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $input = collect(explode("\n", $this->input))
+            ->map(fn ($move) => intval($move))
+            ->toArray();
+
+        $steps = 0;
+        $pointer = 0;
+        while ($pointer < count($input)) {
+            $move = $input[$pointer];
+            $input[$pointer]++;
+            $steps++;
+
+            $pointer += $move;
+        }
+
+        return $steps;
+    }
+
+    /**
+     * Day 05 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        $input = collect(explode("\n", $this->input))
+            ->map(fn ($move) => intval($move))
+            ->toArray();
+
+        $steps = 0;
+        $pointer = 0;
+        while ($pointer < count($input) && $pointer > -1) {
+            $move = $input[$pointer];
+
+            if ($move >= 3) {
+                $input[$pointer]--;
+            } else {
+                $input[$pointer]++;
+            }
+
+            $steps++;
+
+            $pointer += $move;
+        }
+
+        return $steps;
+    }
+}
