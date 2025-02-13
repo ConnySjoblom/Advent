3c919944c98ac2f53a129d4ca93c6094fb93468b
Conny Sjöblom <conny@sjoblom.io>
2025-02-13 16:16:45 +0200

    Add Solution for Year 2017 Day 7 Part 1

---
+++ app/Solutions/Year2017/Day07.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Solutions\Year2017;
+
+use App\Solutions\Solution;
+
+class Day07 extends Solution
+{
+    /**
+     * Day 07 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $input = explode("\n", $this->input);
+
+        $programs = [];
+        $allChildren = [];
+        foreach ($input as $line) {
+            $parts = explode(' -> ', $line);
+
+            $programs[] = str($parts[0])->match('/(.*) /')->toString();
+            $children = array_key_exists(1, $parts) ? explode(', ', $parts[1]) : [];
+
+            $allChildren = array_merge($allChildren, $children);
+        }
+
+        return array_values(array_diff($programs, $allChildren))[0];
+    }
+
+    /**
+     * Day 07 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        return null;
+    }
+}
