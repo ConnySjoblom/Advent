d8213a0c695a8da0b2ef2959d661ad8d61a1d39e
Conny Sjöblom <conny@sjoblom.io>
2025-02-16 18:33:35 +0200

    Add Solution for Year 2024 Day 7 Part 1

---
+++ app/Solutions/Year2024/Day07.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Solutions\Year2024;
+
+use App\Solutions\Solution;
+
+class Day07 extends Solution
+{
+    /**
+     * Day 07 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $lines = explode("\n", trim($this->input));
+
+        $sum = 0;
+        foreach ($lines as $line) {
+            [$target, $numbers] = explode(': ', $line);
+            $target = intval($target);
+            $numbers = array_map('intval', explode(' ', trim($numbers)));
+
+            if ($this->evaluatePossible($target, $numbers)) {
+                $sum += $target;
+            }
+        }
+
+        return $sum;
+    }
+
+    /**
+     * Day 07 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        return null;
+    }
+
+    private function evaluatePossible(int $target, array $numbers): bool
+    {
+        $num = count($numbers);
+        $combinations = 1 << ($num - 1);
+
+        for ($i = 0; $i < $combinations; $i++) {
+            $value = $numbers[0];
+
+            for ($j = 0; $j < $num - 1; $j++) {
+                $operator = ($i & (1 << $j)) ? '*' : '+';
+
+                if ($operator == '+') {
+                    $value += $numbers[$j + 1];
+                } else {
+                    $value *= $numbers[$j + 1];
+                }
+            }
+
+            if ($value == $target) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
