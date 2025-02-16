e395dc1f0e4e003ffb1cb27467caf1c3ce2c0232
Conny Sjöblom <conny@sjoblom.io>
2025-02-16 12:38:23 +0200

    Add Solution for Year 2024 Day 6 Part 2 (if it's stupid and it works, it's not stupid..)

--- app/Solutions/Year2024/Day06.php
+++ app/Solutions/Year2024/Day06.php
@@ -66,6 +66,97 @@ class Day06 extends Solution
      */
     public function partTwo(): string|int|null
     {
-        return null;
+        $map = str($this->input)
+            ->explode("\n")
+            ->map(fn ($line) => str_split($line))
+            ->toArray();
+
+        $origin = $map;
+        $map = $this->walk($map);
+
+        $count = substr_count(implode('', array_map(fn ($line) => implode('', $line), $map)), 'X');
+
+        $a = 0;
+        $loops = 0;
+        foreach ($map as $i => $row) {
+            foreach ($row as $j => $column) {
+                if ($column === 'X') {
+                    $newMap = $origin;
+                    $newMap[$i][$j] = '#';
+
+                    $it = ++$a;
+                    echo "\n$it / $count";
+
+                    if (!$this->walk($newMap)) {
+                        $loops++;
+                    }
+                }
+            }
+        }
+
+        return $loops;
+    }
+
+    private function walk(array $map): array|bool
+    {
+        $directions = [
+            [-1, 0],
+            [0, 1],
+            [1, 0],
+            [0, -1],
+        ];
+
+        $x = 0;
+        $y = 0;
+        foreach ($map as $i => $row) {
+            foreach ($row as $j => $column) {
+                if ($column === '^') {
+                    $y = $i;
+                    $x = $j;
+                }
+            }
+        }
+
+        $ix = $x;
+        $iy = $y;
+
+        $safe = true;
+        $visited = [];
+        while ($safe) {
+            $dy = $y + $directions[0][0];
+            $dx = $x + $directions[0][1];
+
+            if ($x == $ix && $y == $iy) {
+                $map[$y][$x] = '^';
+            } else {
+                $map[$y][$x] = 'X';
+            }
+
+            if (array_key_exists($directions[0][0] . $directions[0][1], $visited)) {
+                if (in_array("$y,$x", $visited[$directions[0][0] . $directions[0][1]])) {
+                    return false;
+                }
+            }
+
+            if (
+                $dy >= count($map) || $dy < 0
+                || $dx >= count($map[0]) || $dx < 0
+            ) {
+                $safe = false;
+                continue;
+            }
+
+            if ($map[$dy][$dx] == '#') {
+                $directions[] = array_shift($directions);
+                continue;
+            }
+
+            $visited[$directions[0][0] . $directions[0][1]][] = "$y,$x";
+
+            $y = $dy;
+            $x = $dx;
+        }
+
+        return $map;
     }
 }
