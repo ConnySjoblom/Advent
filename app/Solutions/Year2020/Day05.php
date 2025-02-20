ea6f8580bf7c83fc08d9ef96c1ba6a92656a6b62
Conny Sjöblom <conny@sjoblom.io>
2025-02-20 22:54:38 +0200

    Add Solution for Year 2020 Day 5

---
+++ app/Solutions/Year2020/Day05.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Solutions\Year2020;
+
+use App\Solutions\Solution;
+
+class Day05 extends Solution
+{
+    /**
+     * Day 05 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $passes = array_map(fn ($pass) => str_split($pass), explode("\n", $this->input));
+
+        $max = 0;
+        foreach ($passes as $pass) {
+            $row = range(0, 127);
+            $col = range(0, 7);
+
+            foreach ($pass as $split) {
+                if ($split == 'F' || $split == 'B') {
+                    $row = match ($split) {
+                        'B' => array_slice($row, intval(count($row) / 2)),
+                        'F' => array_slice($row, 0, intval(count($row) / 2)),
+                    };
+                }
+
+                if ($split == 'L' || $split == 'R') {
+                    $col = match ($split) {
+                        'R' => array_slice($col, intval(count($col) / 2)),
+                        'L' => array_slice($col, 0, intval(count($col) / 2)),
+                    };
+                }
+            }
+
+            $max = max($max, $row[0] * 8 + $col[0]);
+        }
+
+        return $max;
+    }
+
+    /**
+     * Day 05 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        $passes = array_map(fn ($pass) => str_split($pass), explode("\n", $this->input));
+
+        $seatIds = [];
+        foreach ($passes as $pass) {
+            $row = range(0, 127);
+            $col = range(0, 7);
+
+            foreach ($pass as $split) {
+                if ($split == 'F' || $split == 'B') {
+                    $row = match ($split) {
+                        'B' => array_slice($row, intval(count($row) / 2)),
+                        'F' => array_slice($row, 0, intval(count($row) / 2)),
+                    };
+                }
+
+                if ($split == 'L' || $split == 'R') {
+                    $col = match ($split) {
+                        'R' => array_slice($col, intval(count($col) / 2)),
+                        'L' => array_slice($col, 0, intval(count($col) / 2)),
+                    };
+                }
+            }
+
+            $seatIds[] = $row[0] * 8 + $col[0];
+        }
+        sort($seatIds);
+
+        $flipped = array_flip($seatIds);
+
+        for ($i = min($seatIds) + 1; $i <= max($seatIds) - 1; $i++) {
+            if (
+                !array_key_exists($i, $flipped)
+                && array_key_exists($i - 1, $flipped)
+                && array_key_exists($i + 1, $flipped)) {
+                return $i;
+            }
+        }
+
+        return null;
+    }
+}
