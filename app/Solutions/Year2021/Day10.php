6f6dfc8461e444484f9cd6442e8fdd1892c9fcee
Conny Sjöblom <conny@sjoblom.io>
2025-11-23 19:54:51 +0200

    2021 Day 10

---
+++ app/Solutions/Year2021/Day10.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Solutions\Year2021;
+
+use App\Solutions\Solution;
+
+class Day10 extends Solution
+{
+    /**
+     * Day 10 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $input = str($this->input)
+            ->explode("\n")
+            ->map(fn ($line) => str_split($line));
+
+        $score = 0;
+        $openChars = ['(','[', '{', '<'];
+        foreach ($input as $line) {
+
+            $stack = [];
+            foreach ($line as $char) {
+                if (in_array($char, $openChars)) {
+                    $stack[] = $char;
+                    continue;
+                }
+
+                if ($this->convertToOpeningChar($char) == end($stack)) {
+                    array_pop($stack);
+                } else {
+                    $score += $this->scoreIllegalChar($char);
+                    break;
+                }
+            }
+        }
+
+        return $score;
+    }
+
+    /**
+     * Day 10 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        $input = str($this->input)
+            ->explode("\n")
+            ->map(fn ($line) => str_split($line));
+
+        $scores = [];
+        $openChars = ['(','[', '{', '<'];
+        foreach ($input as $line) {
+
+            $stack = [];
+            $corrupted = false;
+            foreach ($line as $char) {
+                if (in_array($char, $openChars)) {
+                    $stack[] = $char;
+                    continue;
+                }
+
+                if ($this->convertToOpeningChar($char) == end($stack)) {
+                    array_pop($stack);
+                } else {
+                    $corrupted = true;
+                    break;
+                }
+            }
+
+            if (!$corrupted && count($stack) > 0) {
+                $lineScore = 0;
+                foreach (array_reverse($stack) as $char) {
+                    $lineScore *= 5;
+                    $lineScore += $this->scoreMissingChar($char);
+                }
+                $scores[] = $lineScore;
+            }
+        }
+
+        sort($scores);
+        return $scores[intdiv(count($scores), 2)];
+    }
+
+    public function convertToOpeningChar($char): string
+    {
+        return match ($char) {
+            ')' => '(',
+            ']' => '[',
+            '}' => '{',
+            '>' => '<',
+        };
+    }
+
+    public function convertToClosingChar($char): string
+    {
+        return match ($char) {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            '<' => '>',
+        };
+    }
+
+    public function scoreIllegalChar($char): int
+    {
+        return match ($char) {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+        };
+    }
+
+    public function scoreMissingChar($char): int
+    {
+        return match ($char) {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
+        };
+    }
+}
---
+++ tests/Unit/Year2021/Day10Test.php
@@ -0,0 +1,45 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    test()->solution = new \App\Solutions\Year2021\Day10();
+});
+
+test('Day 10 Part 1', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partOne());
+})->with([
+    [<<<INPUT
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+INPUT
+        , '26397'],
+]);
+
+test('Day 10 Part 2', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partTwo());
+})->with([
+    [<<<INPUT
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+INPUT
+        , '288957'],
+]);
