7cf7c20e2a42142f2e6fcaa7fc946049075de542
Conny Sjöblom <conny@sjoblom.io>
2025-12-09 20:34:42 +0200

    Add 2025 Day 9 Part 2

--- app/Solutions/Year2025/Day09.php
+++ app/Solutions/Year2025/Day09.php
@@ -3,6 +3,8 @@
 namespace App\Solutions\Year2025;
 
 use App\Solutions\Solution;
+use Carbon\CarbonInterval;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Day09 extends Solution
 {
@@ -11,11 +13,12 @@ class Day09 extends Solution
      */
     public function partOne(): string|int|null
     {
-        $cords = array_map(fn($line) => array_map('intval', explode(',', $line)), explode("\n", $this->input));
+        $cords = array_map(fn ($line) => array_map('intval', explode(',', $line)), explode("\n", $this->input));
+        $cordCount = count($cords);
 
         $maxArea = 0;
-        for ($i = 0; $i < count($cords); $i++) {
-            for ($j = $i + 1; $j < count($cords); $j++) {
+        for ($i = 0; $i < $cordCount; $i++) {
+            for ($j = $i + 1; $j < $cordCount; $j++) {
                 $dx = abs($cords[$i][0] - $cords[$j][0]) + 1;
                 $dy = abs($cords[$i][1] - $cords[$j][1]) + 1;
 
@@ -31,6 +34,63 @@ class Day09 extends Solution
      */
     public function partTwo(): string|int|null
     {
-        return null;
+        $start = now();
+        $cords = array_map(fn ($line) => array_map('intval', explode(',', $line)), explode("\n", $this->input));
+        $cordCount = count($cords);
+
+        $border = [];
+        for ($i = 0; $i < $cordCount; $i++) {
+            [$x, $y] = $cords[$i];
+            [$nx, $ny] = $cords[($i + 1) % $cordCount];
+
+            [$minX, $maxX] = [min($x, $nx), max($x, $nx)];
+            [$minY, $maxY] = [min($y, $ny), max($y, $ny)];
+
+            if ($x == $nx) {
+                for ($yy = $minY; $yy <= $maxY; $yy++) {
+                    $border["$x,$yy"] = true;
+                }
+            } else {
+                for ($xx = $minX; $xx <= $maxX; $xx++) {
+                    $border["$xx,$y"] = true;
+                }
+            }
+        }
+
+        $maxArea = 0;
+        for ($i = 0; $i < $cordCount; $i++) {
+            for ($j = $i + 1; $j < $cordCount; $j++) {
+                [$minX, $maxX] = [min($cords[$i][0], $cords[$j][0]), max($cords[$i][0], $cords[$j][0])];
+                [$minY, $maxY] = [min($cords[$i][1], $cords[$j][1]), max($cords[$i][1], $cords[$j][1])];
+
+                $area = (abs($maxX - $minX) + 1) * (abs($maxY - $minY) + 1);
+
+                if ($area < $maxArea) {
+                    continue;
+                }
+
+                foreach (array_keys($border) as $point) {
+                    [$x, $y] = explode(',', $point);
+                    if ($x > $minX && $x < $maxX && $y > $minY && $y < $maxY) {
+                        continue 2;
+                    }
+                }
+
+                $maxArea = $area;
+            }
+
+            if ($this->verbosity >= OutputInterface::VERBOSITY_VERBOSE) {
+                $elapsed = $start->diffInSeconds(now());
+                $estimatedTotal = $elapsed * $cordCount / ($i + 1);
+                $remaining = $estimatedTotal - $elapsed;
+
+                $elapsed = CarbonInterval::seconds(intval($elapsed))->cascade()->forHumans();
+                $remaining = CarbonInterval::seconds(intval($remaining))->cascade()->forHumans();
+
+                echo "Completed $i / " . ($cordCount - 1) . " | Elapsed: {$elapsed} | Est. remaining: {$remaining}\n";
+            }
+        }
+
+        return $maxArea;
     }
 }
