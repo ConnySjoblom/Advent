57d78477c45157098a29866e26c6752c252da94c
Conny Sjöblom <conny@sjoblom.io>
2025-12-10 19:05:03 +0200

    Add 2021 Day 13

---
+++ app/Solutions/Year2021/Day13.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Solutions\Year2021;
+
+use App\Solutions\Solution;
+
+class Day13 extends Solution
+{
+    /**
+     * Day 13 Part 1
+     */
+    public function partOne($folds = 1): string|int|null
+    {
+        [$dots, $instructions] = explode("\n\n", $this->input);
+
+        $dots = array_fill_keys(explode("\n", $dots), true);
+        $instructions = array_map(fn ($i) => explode('=', explode(' ', $i)[2]), explode("\n", $instructions));
+
+        return $this->fold($dots, $instructions, $folds);
+    }
+
+    /**
+     * Day 13 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        [$dots, $instructions] = explode("\n\n", $this->input);
+
+        $dots = array_fill_keys(explode("\n", $dots), true);
+        $instructions = array_map(fn ($i) => explode('=', explode(' ', $i)[2]), explode("\n", $instructions));
+
+        return $this->fold($dots, $instructions, count($instructions));
+    }
+
+    private function fold(array &$dots, array $instructions, int $folds): int
+    {
+        $visible = [];
+        for ($i = 0; $i < $folds; $i++) {
+            $visible = [];
+
+            [$axis, $n] = $instructions[$i];
+            foreach (array_keys($dots) as $dot) {
+                [$x, $y] = explode(',', $dot);
+
+                if ($axis == 'y') {
+                    if ($y < $n) {
+                        $visible["$x,$y"] = true;
+                    } elseif ($y > $n) {
+                        $newY = $y - (($y - $n) * 2);
+                        $visible["$x,$newY"] = true;
+                    }
+                } else {
+                    if ($x < $n) {
+                        $visible["$x,$y"] = true;
+                    } elseif ($x > $n) {
+                        $newX = $x - (($x - $n) * 2);
+                        $visible["$newX,$y"] = true;
+                    }
+                }
+
+            }
+
+            $dots = $visible;
+        }
+
+        $this->show($visible);
+
+        return count($visible);
+    }
+
+    private function show(array $dots): void
+    {
+        echo "\n\n";
+
+        $maxX = $maxY = 0;
+        foreach (array_keys($dots) as $dot) {
+            [$x,$y] = explode(',', $dot);
+            $maxX = max($maxX, $x);
+            $maxY = max($maxY, $y);
+        }
+
+
+        for ($y = 0; $y <= $maxY; $y++) {
+            for ($x = 0; $x <= $maxX; $x++) {
+                echo array_key_exists("$x,$y", $dots) ? '██' : '  ';
+            }
+
+            echo "\n";
+        }
+
+        echo "\n\n";
+    }
+}
---
+++ tests/Unit/Year2021/Day13Test.php
@@ -0,0 +1,44 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    test()->solution = new \App\Solutions\Year2021\Day13();
+});
+
+test('Day 13 Part 1', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partOne());
+})->with([
+    [<<<INPUT
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
+INPUT
+    , '17'],
+]);
+
+test('Day 13 Part 2', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partTwo());
+})->with([
+    ['', ''],
+])->skip('No test implemented yet.');
