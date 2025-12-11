3f3ae4b60f8beb1f54f70d2150f667d24a5f65a3
Conny Sjöblom <conny@sjoblom.io>
2025-12-11 18:58:51 +0200

    Add Tests for 2024 Day 7

---
+++ tests/Unit/Year2024/Day07Test.php
@@ -0,0 +1,43 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    test()->solution = new \App\Solutions\Year2024\Day07();
+});
+
+test('Day 07 Part 1', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partOne());
+})->with([
+    [<<<INPUT
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+INPUT
+    , '3749'],
+]);
+
+test('Day 07 Part 2', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partTwo());
+})->with([
+    [<<<INPUT
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+INPUT
+        , '11387'],
+])->skip('Solution not implemented.');
