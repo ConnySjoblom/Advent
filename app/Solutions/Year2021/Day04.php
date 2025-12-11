a67bead93d729dbc81453203e987a8791a6a0f40
Conny Sjöblom <conny@sjoblom.io>
2025-12-11 18:14:35 +0200

    Add 2021 Day 4

---
+++ app/Solutions/Year2021/Day04.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Solutions\Year2021;
+
+use App\Solutions\Solution;
+
+class Day04 extends Solution
+{
+    /**
+     * Day 04 Part 1
+     */
+    public function partOne(): string|int|null
+    {
+        $input = explode("\n\n", $this->input);
+        $numbers = explode(',', array_shift($input));
+
+        $boards = [];
+        foreach ($input as $b) {
+            $boards[] = array_map(
+                fn ($line) => preg_split('/\s+/', trim($line)),
+                explode("\n", $b)
+            );
+        }
+
+        for ($i = 0; $i < count($numbers); $i++) {
+            $number = $numbers[$i];
+            foreach ($boards as &$board) {
+                for ($y = 0; $y < count($board); $y++) {
+                    for ($x = 0; $x < count($board[$y]); $x++) {
+                        if ($board[$y][$x] == $number) {
+                            $board[$y][$x] = 'x';
+                        }
+
+                        if ($this->checkBoard($board, $x, $y)) {
+                            return $this->calculateScore($board) * intval($number);
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Day 04 Part 2
+     */
+    public function partTwo(): string|int|null
+    {
+        $input = explode("\n\n", $this->input);
+        $numbers = explode(',', array_shift($input));
+
+        $boards = [];
+        $boardsWon = [];
+        foreach ($input as $b) {
+            $boards[] = array_map(
+                fn ($line) => preg_split('/\s+/', trim($line)),
+                explode("\n", $b)
+            );
+        }
+
+        for ($i = 0; $i < count($numbers); $i++) {
+            $number = $numbers[$i];
+            foreach ($boards as $boardId => &$board) {
+                for ($y = 0; $y < count($board); $y++) {
+                    for ($x = 0; $x < count($board[$y]); $x++) {
+                        if ($board[$y][$x] == $number) {
+                            $board[$y][$x] = 'x';
+                        }
+
+                        if (
+                            $this->checkBoard($board, $x, $y)
+                            && !in_array($boardId, $boardsWon)
+                        ) {
+                            $boardsWon[] = $boardId;
+
+                            if (count($boardsWon) == count($boards)) {
+                                return $this->calculateScore($board) * intval($number);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private function checkBoard(array $board, $nx, $ny): bool
+    {
+        $my = $mx = 0;
+        for ($y = 0; $y < count($board); $y++) {
+            $my += $board[$y][$nx] == 'x' ? 1 : 0;
+        }
+
+        if ($my == count($board)) {
+            return true;
+        }
+
+        for ($x = 0; $x < count($board[$ny]); $x++) {
+            $mx += $board[$ny][$x] == 'x' ? 1 : 0;
+        }
+
+        if ($mx == count($board[$ny])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function calculateScore(mixed $board): int
+    {
+        $score = 0;
+        for ($y = 0; $y < count($board); $y++) {
+            for ($x = 0; $x < count($board[$y]); $x++) {
+                $score += $board[$y][$x] != 'x' ? $board[$y][$x] : 0;
+            }
+        }
+
+        return $score;
+    }
+}
---
+++ tests/Unit/Year2021/Day04Test.php
@@ -0,0 +1,63 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    test()->solution = new \App\Solutions\Year2021\Day04();
+});
+
+test('Day 04 Part 1', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partOne());
+})->with([
+    [<<<INPUT
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+INPUT
+    , '4512'],
+]);
+
+test('Day 04 Part 2', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partTwo());
+})->with([
+    [<<<INPUT
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+INPUT
+        , '1924'],
+]);
