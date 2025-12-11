dc07c2c1c426d27c44d83e2194eeaf7ed7b68b50
Conny Sjöblom <conny@sjoblom.io>
2025-12-11 18:50:35 +0200

    Add Tests for 2024 Day 2

---
+++ tests/Unit/Year2024/Day02Test.php
@@ -0,0 +1,37 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    test()->solution = new \App\Solutions\Year2024\Day02();
+});
+
+test('Day 02 Part 1', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partOne());
+})->with([
+    [<<<INPUT
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+INPUT
+    , '2'],
+]);
+
+test('Day 02 Part 2', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partTwo());
+})->with([
+    [<<<INPUT
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+INPUT
+        , '4'],
+]);
