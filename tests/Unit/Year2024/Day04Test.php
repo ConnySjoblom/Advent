09599c66e195e32f7f90e8980dfc6149b65f6533
Conny Sjöblom <conny@sjoblom.io>
2025-12-11 18:52:20 +0200

    Add Tests for 2024 Day 4

---
+++ tests/Unit/Year2024/Day04Test.php
@@ -0,0 +1,45 @@
+<?php
+
+use function PHPUnit\Framework\assertEquals;
+
+beforeEach(function () {
+    test()->solution = new \App\Solutions\Year2024\Day04();
+});
+
+test('Day 04 Part 1', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partOne());
+})->with([
+    [<<<INPUT
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+INPUT
+    , '18'],
+]);
+
+test('Day 04 Part 2', function (string $input, string $answer) {
+    test()->solution->input = $input;
+    assertEquals($answer, test()->solution->partTwo());
+})->with([
+    [<<<INPUT
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+INPUT
+        , '9'],
+]);
